279ffd11d867f2e420301ab292a04f1a904c3e82
poly <polycste@gmail.com>
2015-05-09 21:46:05 +0000

    added header and footer

---
+++ footer.php
@@ -0,0 +1,66 @@
+	<!-- FOOTER -->
+    <div class="clearfix"></div>
+	<footer id="footer" class="">
+	 	    	        
+        
+             <div class="colophon ">
+                <div class="container ">
+                    <div class="row-fluid">
+                        <div class="span4 social-block pull-left"><a class="" href="http://sevenhillrestaurant.net/feed/rss/" title="Suscribe to my rss feed"  ></a></div><div class="span4 credits"><p> &middot; &copy; 2015 <a href="index.php" title="sevenhillrestaurant" rel="bookmark">sevenhillrestaurant</a> &middot; Designed by DK Technology</p></div><div class="span4 backtop"><p class="pull-right"><a class="back-to-top" href="index.php#">Back to top</a></p></div>                    </div><!-- .row-fluid -->
+                </div><!-- .container -->
+            </div><!-- .colophon -->
+       
+        
+    		</footer>
+<!-- begin visitor maps  -->
+<script type="text/javascript">
+//<![CDATA[
+function wo_map_console(url) {
+  window.open(url,"wo_map_console","height=650,width=800,toolbar=no,statusbar=no,scrollbars=yes").focus();
+}
+//]]>
+</script>
+<!-- end visitor maps -->
+<script type='text/javascript' src='http://sevenhillrestaurant.net/wp-includes/js/jquery/ui/jquery.ui.core.min.js?ver=1.10.4'></script>
+<script type='text/javascript' src='http://sevenhillrestaurant.net/wp-includes/js/jquery/ui/jquery.ui.datepicker.min.js?ver=1.10.4'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var slider = {"effect":"fade","delay":"5000","duration":"600","start":"1"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='wp-content/plugins/responsive-slider/responsive-slider.js%3Fver=0.1'></script>
+<script type='text/javascript' src='http://sevenhillrestaurant.net/wp-includes/js/comment-reply.min.js?ver=3.9.6'></script>
+<script type='text/javascript' src='wp-content/themes/customizr/inc/js/bootstrap.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var TCParams = {"FancyBoxState":"1","FancyBoxAutoscale":"1","SliderName":"demo","SliderDelay":"5000","SliderHover":"1","SmoothScroll":"linear"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='wp-content/themes/customizr/inc/js/tc-scripts.min.js'></script>
+<script type='text/javascript' src='wp-content/themes/customizr/inc/js/holder.js'></script>
+<script type='text/javascript' src='wp-content/themes/customizr/inc/js/fancybox/jquery.fancybox-1.3.4.min.js'></script>
+<script type='text/javascript' src='wp-content/themes/customizr/inc/js/retina.min.js'></script>
+    <!-- SideOffer -->
+	<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		/* SideOffer Sidebar Functionality */
+		$("#sideoffer").toggle(
+			function() { $(this).animate({ "right": "-50px" }, "slow"); },
+			function() { $(this).animate({ "right": "-501px" }, "slow"); }
+		);
+		
+		/* SideOffer .hd-sideoffer click function */
+		$(".sideoffer").click(function(){ $("#sideoffer").click(); });
+		/* SideOffer aLlow clicks on content box */
+		$("#sideoffer .box").click(function(event){ event.stopPropagation(); });
+	});
+	</script>
+	<div id="sideoffer">
+	<div class="box">
+	<h5></h5>    <a href="wp-content/uploads/2014/09/1_1.jpg"><img src="wp-content/uploads/2014/09/1_1_1.jpg" />Click here<br></a>
+
+<a href="wp-content/uploads/2014/09/2_2.jpg"><img src="wp-content/uploads/2014/09/2_2_2.jpg" />Click here<br></a>	</div><!-- End .box  -->
+	</div><!-- End #sideoffer -->
+    <!-- End SideOffer -->
+    	</body>
+</html>
